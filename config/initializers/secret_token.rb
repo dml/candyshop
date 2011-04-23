e451a66a9a070752ef6809ac84e06c530214e241
Dmitry Larkin <dmitry.larkin@gmail.com>
2011-04-23 20:31:10 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CandyShop::Application.config.secret_token = '68cd62f77b64d1fa481874324dda4cbe50bd5cc16600c604cba4bbed5defc67bc3c600da83f552a8d99b889ddc3320ad2bbbf837ef4e3fb5961b249c153e34e3'
